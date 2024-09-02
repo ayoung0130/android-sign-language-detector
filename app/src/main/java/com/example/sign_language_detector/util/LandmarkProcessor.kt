6a00e7825bef0d91c15c2fa6eabde10ed0acaccc
Jung a young <mshof0130@naver.com>
2024-09-03 01:56:48 +0900

    각도 계산 수정 및 중복 개선

--- app/src/main/java/com/example/sign_language_detector/util/LandmarkProcessor.kt
+++ app/src/main/java/com/example/sign_language_detector/util/LandmarkProcessor.kt
@@ -10,6 +10,7 @@ import kotlin.math.sqrt
 
 class LandmarkProcessor {
 
+    // 포즈 랜드마크 중 사용할 인덱스 리스트
     private val poseLandmarkIndices = listOf(
         0, 2, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
     )
@@ -23,21 +24,18 @@ class LandmarkProcessor {
         val jointRightHands = Array(21) { FloatArray(3) }
         val jointPose = Array(21) { FloatArray(3) }
 
+        // 손 랜드마크 처리
         handResultBundle.results.forEach { result ->
             result.landmarks().forEachIndexed { i, hand ->
-                // 손의 핸디드니스에 따라 적절한 배열에 값을 저장
-                if (result.handedness()[i].first().categoryName() == "Left") {
-                    hand.forEachIndexed { j, lm ->
-                        jointRightHands[j] = floatArrayOf(lm.x(), lm.y(), lm.z())
-                    }
-                } else {
-                    hand.forEachIndexed { j, lm ->
-                        jointLeftHands[j] = floatArrayOf(lm.x(), lm.y(), lm.z())
-                    }
+                val isLeftHand = result.handedness()[i].first().categoryName() == "Left"
+                hand.forEachIndexed { j, lm ->
+                    val jointArray = if (isLeftHand) jointRightHands else jointLeftHands
+                    jointArray[j] = floatArrayOf(lm.x(), lm.y(), lm.z())
                 }
             }
         }
 
+        // 포즈 랜드마크 처리
         poseResultBundle.results.forEach { result ->
             result.landmarks().forEachIndexed { _, pose ->
                 pose.forEachIndexed { j, lm ->
@@ -50,6 +48,7 @@ class LandmarkProcessor {
             }
         }
 
+        // 각도 계산
         val leftHandAngle = angleHands(jointLeftHands)
         val rightHandAngle = angleHands(jointRightHands)
         val poseAngle = anglePose(jointPose)
@@ -60,32 +59,51 @@ class LandmarkProcessor {
                 jointPose.flatMap { it.toList() }).toFloatArray()
 
         val data = (jointData + leftHandAngle + rightHandAngle + poseAngle)
-        Log.d("tag", "data size: ${data.size}")
+        Log.d("LandmarkProcessor", "data size: ${data.size}")
 
         return data
     }
 
-    // 각도 처리
+    // 손 각도 처리
     private fun angleHands(jointHands: Array<FloatArray>): FloatArray {
         val v1Indices = arrayOf(0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19)
         val v2Indices =
             arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+        val firstAngleIndices = arrayOf(0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18)
+        val secondAngleIndices = arrayOf(1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19)
 
-        return calculateAngles(jointHands, v1Indices, v2Indices)
+        return calculateAngles(
+            jointHands,
+            v1Indices,
+            v2Indices,
+            firstAngleIndices,
+            secondAngleIndices
+        )
     }
 
+    // 포즈 각도 처리
     private fun anglePose(jointPose: Array<FloatArray>): FloatArray {
-        val v1Indices = arrayOf(0, 2, 0, 1, 0, 0, 7, 8, 8, 8, 10, 12, 12, 12, 7, 7, 9, 11, 11, 11)
-        val v2Indices =
-            arrayOf(2, 4, 1, 3, 5, 6, 8, 7, 10, 20, 12, 14, 16, 18, 9, 19, 11, 13, 15, 17)
+        val v1Indices = arrayOf(0, 1, 0, 5, 7, 9, 11, 11, 11, 0, 2, 0, 6, 8, 10, 12, 12, 12, 7, 19)
+        val v2Indices = arrayOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 2, 4, 6, 8, 10, 12, 14, 16, 18, 8, 7)
+        val firstAngleIndices = arrayOf(0, 2, 3, 4, 5, 5, 5, 9, 11, 12, 13, 14, 14, 14, 18)
+        val secondAngleIndices = arrayOf(1, 3, 4, 5, 6, 7, 8, 10, 12, 13, 14, 15, 16, 17, 19)
 
-        return calculateAngles(jointPose, v1Indices, v2Indices)
+        return calculateAngles(
+            jointPose,
+            v1Indices,
+            v2Indices,
+            firstAngleIndices,
+            secondAngleIndices
+        )
     }
 
+    // 각도 계산 함수
     private fun calculateAngles(
         joints: Array<FloatArray>,
         v1Indices: Array<Int>,
-        v2Indices: Array<Int>
+        v2Indices: Array<Int>,
+        firstAngleIndices: Array<Int>,
+        secondAngleIndices: Array<Int>,
     ): FloatArray {
         // 각 관절 간의 벡터 계산
         val v = Array(v1Indices.size) { i ->
@@ -93,29 +111,23 @@ class LandmarkProcessor {
                 joints[v2Indices[i]][j] - joints[v1Indices[i]][j]
             }
         }
+        val angles = FloatArray(15)
 
         // 벡터 정규화 및 각도 계산
-        val angles = FloatArray(15)
-        val angleIndices = arrayOf(
-            0, 1, 2, 4, 5, 6, 8, 9, 10,
-            12, 13, 14, 16, 17, 18
-        )
+        for (i in angles.indices) {
+            val v1 = v[firstAngleIndices[i]]
+            val v2 = v[secondAngleIndices[i]]
 
-        for (i in angleIndices.indices) {
-            val idx = angleIndices[i]
-            val v1 = v[idx]
-            val v2 = v[idx + 1]
-
+            // 벡터의 크기(노름) 계산
             val normV1 = sqrt(v1.map { it * it }.sum())
             val normV2 = sqrt(v2.map { it * it }.sum())
 
-            if (normV1 == 0f || normV2 == 0f) {
-                angles[i] = 0f
+            angles[i] = if (normV1 == 0f || normV2 == 0f) {
+                0f
             } else {
-                val dotProduct =
-                    v1.zip(v2).sumOf { (a, b) -> (a * b).toDouble() }.toFloat() / (normV1 * normV2)
-                val clippedDotProduct = dotProduct.coerceIn(-1f, 1f)
-                angles[i] = acos(clippedDotProduct)
+                // 도트 곱 계산, acos으로 각도 계산
+                val dotProduct = v1.zip(v2).map { (a, b) -> a * b }.sum() / (normV1 * normV2)
+                acos(dotProduct.coerceIn(-1f, 1f))
             }
         }
 
