f895a002e26fd829a28e800131e72f60b2c09973
Jung a young <mshof0130@naver.com>
2024-09-24 02:52:58 +0900

    전역 변수 설정

---
+++ app/src/main/java/com/ayeong/sign_language_detector/Constants.kt
@@ -0,0 +1,7 @@
+package com.ayeong.sign_language_detector
+
+object Constants {
+    const val SLICING_WINDOW = 20
+    const val JUMPING_WINDOW = 10
+    val ACTIONS = listOf("가렵다", "기절", "부러지다", "어제", "어지러움", "열나다", "오늘", "진통제", "창백하다", "토하다")
+}
